8f932c8b857589601a0486ebe8fd1e84902a4f27
FletcherMeyer <53588444+FletcherMeyer@users.noreply.github.com>
2025-10-27 21:25:17 -0500

    Download Splunk forwarder in debian_forwarder.sh

---
+++ debian_forwarder.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~
+wget -O splunk_forwarder.deb wget "https://download.splunk.com/products/universalforwarder/releases/10.0.1/linux/splunkforwarder-10.0.1-c486717c322b-linux-amd64.deb"
